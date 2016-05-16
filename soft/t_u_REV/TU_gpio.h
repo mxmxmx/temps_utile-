a5aec2c703f59f52a08f6b6ec9992328e088fc71
mxmxmx <mxxstadler@gmail.com>
2016-05-16 09:05:11 +0200

    add version switch

--- soft/t_u_REV/TU_gpio.h
+++ soft/t_u_REV/TU_gpio.h
@@ -1,6 +1,12 @@
 #ifndef TU_GPIO_H_
 #define TU_GPIO_H_
 
+/*  
+ * uncomment L#8 if using revision 0 pcbs (typically, yellow pcbs): 
+ */
+ 
+//#define _TEMPS_UTILE_REV_0
+
 #define CV1 A5
 #define CV2 A4
 #define CV3 A6
@@ -9,19 +15,39 @@
 #define TR1 0
 #define TR2 1 // reset
 
-#define CLK1 5
-#define CLK2 6
-#define CLK3 7
-#define CLK4 A14 // alt = 29
-#define CLK5 8
-#define CLK6 2
+// outputs, tact buttons:
+#ifdef _TEMPS_UTILE_REV_0
+  #define CLK1 29
+  #define CLK2 28
+  #define CLK3 10
+  #define CLK4 A14
+  #define CLK5 2
+  #define CLK6 9
+  
+  #define but_top 5
+  #define but_bot 4 
+#else
+  #define CLK1 5
+  #define CLK2 6
+  #define CLK3 7
+  #define CLK4 A14 // DAC channel; alt = 29
+  #define CLK5 8
+  #define CLK6 2
+  
+  #define but_top 3
+  #define but_bot 12
+#endif
 
-#define but_top 3
-#define but_bot 12
-
-#define OLED_DC 9
-#define OLED_RST 4
-#define OLED_CS 10
+// OLED com: 
+#ifdef _TEMPS_UTILE_REV_0
+  #define OLED_DC 6
+  #define OLED_RST 7
+  #define OLED_CS 8
+#else
+  #define OLED_DC 9
+  #define OLED_RST 4
+  #define OLED_CS 10
+#endif
 
 // OLED CS is active low
 #define OLED_CS_ACTIVE LOW
