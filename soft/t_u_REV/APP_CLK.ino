d6e7bc583634aeaa8671ae571749caeac24e6fbb
mxmxmx <mxxstadler@gmail.com>
2016-08-25 23:24:36 +0200

    transition more gently

--- soft/t_u_REV/APP_CLK.ino
+++ soft/t_u_REV/APP_CLK.ino
@@ -38,6 +38,8 @@ const uint16_t TOGGLE_THRESHOLD = 500; // ADC threshold for 0/1 parameters (~1.2
 const uint32_t SCALE_PULSEWIDTH = 58982; // 0.9 for signed_multiply_32x16b
 const uint32_t TICKS_TO_MS = 43691; // 0.6667f : fraction, if TU_CORE_TIMER_RATE = 60 us : 65536U * ((1000 / TU_CORE_TIMER_RATE) - 16)
 const uint32_t TICK_JITTER = 0xFFFFFFF; // 1/16 : threshold/double triggers reject -> ext_frequency_in_ticks_
+const uint32_t TICK_SCALE  = 0xE6666666; // 0.9 for signed_multiply_32x32
+                        
 
 extern const uint32_t BPM_microseconds_4th[];
 
@@ -660,6 +662,7 @@ public:
         prev_channel_frequency_in_ticks_ = channel_frequency_in_ticks_;
         channel_frequency_in_ticks_ = multiply_u32xu32_rshift32(ext_frequency_in_ticks_, multipliers_[_multiplier]) << 3; 
         tickjitter_ = multiply_u32xu32_rshift32(channel_frequency_in_ticks_, TICK_JITTER);
+        prev_channel_frequency_in_ticks_ = multiply_u32xu32_rshift32(prev_channel_frequency_in_ticks_, TICK_SCALE);
      }
      // limit to > 0
      if (!channel_frequency_in_ticks_)  
@@ -694,7 +697,7 @@ public:
          // if so, reset ticks: 
          subticks_ = 0x0;
          // count, only if ... 
-         if (_subticks < tickjitter_ || _subticks < (prev_channel_frequency_in_ticks_ >> 1)) // reject, if jittery or skip quasi-double triggers when ext. frequency changes...
+         if (_subticks < tickjitter_ || _subticks < prev_channel_frequency_in_ticks_) // reject, if jittery or skip quasi-double triggers when ext. frequency changes...
             return;
          clk_cnt_++;  
          _output = gpio_state_ = process_clock_channel(_mode); // = either ON, OFF, or anything (DAC)
